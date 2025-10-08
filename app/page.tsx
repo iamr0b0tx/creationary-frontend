9d4e0498bb9c0d6b9d4371c28637f91ec916127c
eniola <eniolafaithful57@gmail.com>
2025-10-08 08:17:25 +0100

    Setup Tests
    Installed vitest for testing, and setup a basic test "suite" for the home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
-import Image from "next/image";
 
 export default function Home() {
-  return <div className="">Start Project </div>;
+  return (
+    <div className="">
+      <h1>Creationary</h1>
+    </div>
+  );
 }
