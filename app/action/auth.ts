f122748fca8763d6b10ccdebc02fa57622dc224c
eniola <eniolafaithful57@gmail.com>
2025-10-15 13:56:27 +0100

    Improve Authentication management
    - I was not managing user session on signin and signup previously.
    - I was not handling redirect after successfully sigining in and signing out.
    - There was also need to access the the api base_url from a central location for easy access throughout the entire application.
    
    All these have been catered for in the commit

--- app/action/auth.ts
+++ app/action/auth.ts
@@ -2,13 +2,19 @@
 
 import z from "zod";
 import { loginFormSchema, signUpFormSchema } from "@/lib/definitions";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
 export async function handleEmailLogin(prev: unknown, formData: FormData) {
   const email = formData.get("email");
   const password = formData.get("password");
   const lastName = formData.get("lastName");
 
-  const validatedFields = loginFormSchema.safeParse({ email, password, lastName });
+  const validatedFields = loginFormSchema.safeParse({
+    email,
+    password,
+    lastName,
+  });
 
   if (!validatedFields.success) {
     console.error("Validation errors:", validatedFields.error.flatten());
@@ -16,23 +22,28 @@ export async function handleEmailLogin(prev: unknown, formData: FormData) {
   }
 
   try {
-     const res = await fetch("http://placeholder", {
-       method: "POST",
-       headers: {
-         "Content-Type": "application/json",
-       },
-       body: JSON.stringify(validatedFields.data),
-     });
+    const res = await fetch(`${process.env.BASE_URL}/auth/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(validatedFields.data),
+    });
 
-     if (!res.ok) {
-       throw new Error("Login failed");
-     }
+    if (!res.ok) {
+      throw new Error("Login failed");
+    }
 
-     const data = await res.json();
-     console.log("Login successful:", data);
-  }catch(error){
+    const responseData = await res.json();
+    createCookie("token", responseData.token, { httpOnly: true, path: "/" });
+    createCookie("user", JSON.stringify({ ...responseData.user }), {
+      httpOnly: false,
+      path: "/",
+    });
+  } catch (error) {
     console.error("Error during login:", error);
   }
+  redirect("/dashboard");
 }
 
 export async function handleRegister(prev: unknown, formData: FormData) {
@@ -40,7 +51,11 @@ export async function handleRegister(prev: unknown, formData: FormData) {
   const password = formData.get("password");
   const confirmPassword = formData.get("confirmPassword");
 
-  const validatedFields = signUpFormSchema.safeParse({ email, password, confirmPassword });
+  const validatedFields = signUpFormSchema.safeParse({
+    email,
+    password,
+    confirmPassword,
+  });
 
   if (!validatedFields.success) {
     console.error("Validation errors:", validatedFields.error.flatten());
@@ -48,23 +63,56 @@ export async function handleRegister(prev: unknown, formData: FormData) {
   }
 
   try {
-     const res = await fetch("http://placeholder", {
-       method: "POST",
-       headers: {
-         "Content-Type": "application/json",
-       },
-       body: JSON.stringify(validatedFields.data),
-     });
+    const res = await fetch(`${process.env.BASE_URL}/auth/register`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(validatedFields.data),
+    });
 
-     if (!res.ok) {
-       throw new Error("Login failed");
-     }
+    if (!res.ok) {
+      throw new Error("Login failed");
+    }
 
-     const data = await res.json();
-     console.log("Login successful:", data);
-  }catch(error){
+    const responseData = await res.json();
+    createCookie("token", responseData.token, { httpOnly: true, path: "/" });
+    createCookie("user", JSON.stringify({ ...responseData.user }), {
+      httpOnly: false,
+      path: "/",
+    });
+  } catch (error) {
     console.error("Error during login:", error);
   }
+  redirect("/dashboard");
 }
 
 export async function handleProviderLogin(provider: string) {}
+
+export async function getSession() {
+  try {
+    const user = (await cookies()).get("user")?.value;
+
+    if (!user) redirect("/auth");
+
+    const userInfo = JSON.parse(user);
+    return userInfo;
+  } catch (error) {
+    redirect("/auth");
+  }
+}
+
+async function createCookie(
+  name: string,
+  value: string,
+  options: { httpOnly?: boolean; path?: string } = {}
+) {
+  (await cookies()).set({
+    name,
+    value: value,
+    httpOnly: options.httpOnly || false,
+    expires: new Date(Date.now() + 5 * 24 * 60 * 60 * 1000), // 5 days
+    sameSite: "strict",
+    secure: process.env.NODE_ENV === "production",
+  });
+}
