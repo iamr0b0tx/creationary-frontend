e3aab268a21c0bf3935dc039040a164ec5e57efa
eniola <eniolafaithful57@gmail.com>
2025-10-24 15:43:47 +0100

    Setup user Profile Page
    This page consists of all of the information that one might need regarding a Creator/seller
    
    Profile Image, Products, Reviews, Follower Count, Messaging, Social Media Contact, Revies, Bio and the likes.
    This ensures that every Seller has a central place where all of their details are available

---
+++ app/creator/[id]/page.tsx
@@ -0,0 +1,12 @@
+// Mock creator data - this would come from API based on username
+
+import UserProfileContent from "./ProfilePage";
+
+export default async function UserProfilePage({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const id = parseInt((await params).id, 10);
+  return <UserProfileContent id={id} />;
+}
