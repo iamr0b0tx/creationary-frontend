de98efb48c7c407a9253ae5b7ae4c73fa8e6e2bb
eniola <eniolafaithful57@gmail.com>
2025-10-30 09:58:19 +0100

    Refactor user cookie retrieval for improved readability, and efficiency

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,8 @@ import { cookies } from "next/headers";
 import HomePageComponent from "./HomepageComponent";
 
 export default async function HomePage() {
-  const user = (await cookies()).get("user")?.value
-    ? JSON.parse((await cookies()).get("user")!.value)
-    : null;
-  
+  const userCookie = (await cookies()).get("user")?.value;
+  const user = userCookie ? JSON.parse(userCookie) : null;
+
   return <HomePageComponent user={user} />;
 }
